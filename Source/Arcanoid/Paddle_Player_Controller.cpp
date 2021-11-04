dd9f9619e9e685b2bbcb84cb5af7c0714da10bb3
MAGICFACTORY\agoncharov <gonas.hans@gmail.com>
2021-11-04 18:59:29 +0300

    Add player controller and inputs

--- Source/Arcanoid/Paddle_Player_Controller.cpp
+++ Source/Arcanoid/Paddle_Player_Controller.cpp
@@ -2,4 +2,52 @@
 
 
 #include "Paddle_Player_Controller.h"
+#include "Kismet/GameplayStatics.h"
+#include "Camera/CameraActor.h"
 
+#include "Paddle.h"
+//#include "Ball.h"
+
+APaddle_Player_Controller::APaddle_Player_Controller()
+{
+	
+}
+
+void APaddle_Player_Controller::BeginPlay()
+{
+	
+	//Super::BeginPlay();
+
+	TArray<AActor*> CameraActors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACameraActor::StaticClass(), CameraActors);
+
+	FViewTargetTransitionParams Params;
+	SetViewTarget(CameraActors[0], Params);
+	
+	
+}
+
+void APaddle_Player_Controller::SetupInputComponent()
+{
+	
+	Super::SetupInputComponent();
+
+	EnableInput(this);
+
+	InputComponent->BindAxis("MoveHorizontal", this, &APaddle_Player_Controller::MoveHorizontal);
+	
+}
+
+void APaddle_Player_Controller::MoveHorizontal(float AxisValue)
+{
+
+	auto MyPawn = Cast<APaddle>(GetPawn());
+
+	if (MyPawn)
+	{
+		
+		MyPawn->MoveHorizontal(AxisValue);
+		
+	}
+	
+}
--- Source/Arcanoid/Paddle_Player_Controller.h
+++ Source/Arcanoid/Paddle_Player_Controller.h
@@ -6,12 +6,28 @@
 #include "GameFramework/PlayerController.h"
 #include "Paddle_Player_Controller.generated.h"
 
-/**
- * 
- */
+// ball class
+
+
 UCLASS()
 class ARCANOID_API APaddle_Player_Controller : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	
+	APaddle_Player_Controller();
+
+	UFUNCTION()
+	virtual void SetupInputComponent() override;
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	void MoveHorizontal(float AxisValue);
+
+	//ball references
+	
 	
 };
